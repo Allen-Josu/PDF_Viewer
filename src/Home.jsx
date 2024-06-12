a56fade3e079e832bc4e04b6dab1ff65b3f8379c
Allen Joseph Joy <allenalackaparambil@gmail.com>
2024-06-12 16:00:43 +0530

    Finished Code

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,11 @@
-import { useNavigate } from "react-router-dom";
-
 
 function Home() {
-    const navigate = useNavigate()
 
-    const pdf = () => {
-        navigate("/student-declaration")
-    }
     return (
         <>
-            <button className="btn btn-success" target="_blank" onClick={pdf}>
+            <a className="btn btn-success" target="_blank" href="/student-declaration">
                 PDF Viewer
-            </button>
+            </a>
         </>
     )
 }
