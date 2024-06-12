fcc4519cea5a3e8db07c14677380098512cb50d7
Allen Joseph Joy <allenalackaparambil@gmail.com>
2024-06-12 15:43:36 +0530

    Finished

--- src/components/PDF.jsx
+++ src/components/PDF.jsx
@@ -1,24 +1,41 @@
-import { Document, Page, StyleSheet, Text, View } from '@react-pdf/renderer'
-
+import { Document, Font, Page, StyleSheet, Text, View } from '@react-pdf/renderer'
+Font.register({ family: 'Times-Roman' });
 
 const styles = new StyleSheet.create({
     section: {
-        margin: "30px 30px",
+        margin: "0px 40px 0px 40px",
         display: "flex",
         alignItems: "center",
+        justifyContent: "center",
     },
     heading: {
-        fontSize: "12px",
-        fontWeight: "900",
+        fontSize: "16px",
+        letterSpacing: "3px",
         textTransform: "uppercase",
-        marginBottom: "10px"
+        marginTop: "15vh",
+        fontFamily: "Times-Roman"
     },
-    subheading: {
-        marginTop: "20px",
-        textTransform: 'uppercase',
-        fontSize: "12px"
+    content: {
+        textAlign: "justify",
+        fontSize: "13px",
+        letterSpacing: "3px",
+        margin: "30px 10px 0px 10px",
+        lineHeight: "2",
+    },
+    footer: {
+        margin: "20px 40px 0px 40px",
+
+    },
+    signature: {
+        fontSize: "11px",
+        letterSpacing: "3px",
+        lineHeight: "2",
+        margin: "0px 10px",
+
     }
 })
+
+
 
 function PDF() {
     return (
@@ -26,8 +43,19 @@ function PDF() {
             <Document>
                 <Page size="A4" styles={styles.pages}>
                     <View style={styles.section}>
-                        <Text>
-                            hello
+                        <Text style={styles.heading}>
+                            Declaration Statement
+                        </Text>
+                        <Text style={styles.content}>
+                            This is to certify that [Student Full Name], with admission number [Admission Number], has successfully completed driver education at [Driving School Name].
+                        </Text>
+                    </View>
+                    <View style={styles.footer}>
+                        <Text style={styles.signature}>
+                            12-06-2024
+                        </Text>
+                        <Text style={styles.signature}>
+                            Cherthala
                         </Text>
                     </View>
                 </Page>
@@ -37,3 +65,4 @@ function PDF() {
 }
 
 export default PDF;
+
